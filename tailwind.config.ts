92ae10721180db3ecf909675cefd683bfde6eb43
nousra0 <raissouninossra@gmail.com>
2023-10-21 13:28:07 +0300

    deploy web

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,7 +33,7 @@ module.exports = {
         'bg-img-1': "url('./img-1.png')",
         'bg-img-2': "url('./img-2.png')",
         'feature-bg': "url('./feature-bg.png')",
-        pattern: "url('./pattern.png')",
+        'pattern': "url('./pattern.png')",
         'pattern-2': "url('./pattern-bg.png')",
       },
       screens: {
