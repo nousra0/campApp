5a0d3043fa9da8e1905d9636c6b00e39d21eb5d9
nousra0 <raissouninossra@gmail.com>
2023-10-21 10:36:16 +0300

    deploy

---
+++ dist/_next/static/chunks/app/layout-98e32a2489b7e533.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{6e3:function(e,n,t){Promise.resolve().then(t.t.bind(t,2853,23)),Promise.resolve().then(t.t.bind(t,413,23)),Promise.resolve().then(t.t.bind(t,8326,23))},2853:function(){}},function(e){e.O(0,[413,326,971,472,744],function(){return e(e.s=6e3)}),_N_E=e.O()}]);
