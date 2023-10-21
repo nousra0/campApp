e43dc3dffb7f0d29a2bdcf0d7c57d6129c3640a2
nousra0 <raissouninossra@gmail.com>
2023-10-21 09:48:07 +0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        appDir: true,
-      },
-      output: 'export',
-      basePath: '/camp-app'
-}
+const nextConfig = {}
 
 module.exports = nextConfig
